de125d8328226bc71fe3cd4b95a163dcbb66db3b
AnikIqbal <anik.iqbal@outlook.com>
2017-06-17 22:43:43 +0600

    Color text using escape characters

--- Ch_N/COLORTXT.C
+++ Ch_N/COLORTXT.C
@@ -1,11 +1,24 @@
 #include <stdio.h>
-#include <conio.h>
-#include <dos.h>
 
+#define RED      "\x1B[31m"
+#define GREEN    "\x1B[32m"
+#define YELLOW   "\x1B[33m"
+#define BLUE     "\x1B[34m"
+#define MAGENTA  "\x1B[35m"
+#define CYAN     "\x1B[36m"
+#define WHITE    "\x1B[37m"
+#define RESET    "\x1B[0m"
+
+#define B_RED      "\x1B[41m"
+#define B_GREEN    "\x1B[42m"
+#define B_YELLOW   "\x1B[43m"
+#define B_BLUE     "\x1B[44m"
+#define B_MAGENTA  "\x1B[45m"
+#define B_CYAN     "\x1B[46m"
+#define B_WHITE    "\x1B[47m"
+#define B_RESET    "\x1B[0m"
 int main(){
-   clrscr();
-   textcolor(RED);
-   cprintf("Monami is Cute.\n");
-   sleep(2);
-return 0;
+   printf(B_GREEN RED "Monami is Cute.\n" RESET);
+   
+    return 0;
 }
