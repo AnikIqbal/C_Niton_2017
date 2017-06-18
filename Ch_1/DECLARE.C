b2be48815b37385b98d65df37a1ba87e2a1b9a31
AnikIqbal <anik.iqbal@outlook.com>
2017-06-18 13:41:17 +0600

    effect of undeclared var

---
+++ Ch_1/DECLARE.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1;
+    int num2;
+    a = 5;
+    b = 15;
+
+    sum = num1 + num2; // variable sum hasn't been declared previously so it is undefined
+
+    printf("sum = %d",sum);
+
+    return 0;
+
+}
